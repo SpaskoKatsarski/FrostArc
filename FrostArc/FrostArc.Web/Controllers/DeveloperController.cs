887ae7e8fa77dd6ed12a8d44a290f407e9103b3e
Spasko <patsokatsarski1@gmail.com>
2023-12-10 12:42:51 +0200

    Adding dev controller

---
+++ FrostArc/FrostArc.Web/Controllers/DeveloperController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostArc.Web.Controllers
+{
+    //TODO Implement
+    public class DeveloperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
